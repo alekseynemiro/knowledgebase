14e9d90df4f80f387168a81e9cb9b86274e517cb
Aleksey Nemiro <aleksey@kbyte.ru>
2025-01-04 11:17:17 +0300

    Create DocList component

---
+++ src/components/DocList/index.tsx
@@ -0,0 +1,47 @@
+import React, { type ReactNode } from 'react';
+import clsx from 'clsx';
+import {
+  useCurrentSidebarCategory,
+  useDocById,
+  filterDocCardListItems,
+  findFirstSidebarItemLink,
+} from '@docusaurus/plugin-content-docs/client';
+import type { Props } from '@theme/DocCardList';
+
+function DocListForCurrentSidebarCategory({className}: Props) {
+  const category = useCurrentSidebarCategory();
+
+  return (
+    <DocList items={category.items} className={className} />
+  );
+}
+
+export default function DocList(props: Props): ReactNode {
+  const { items, className } = props;
+
+  if (!items) {
+    return (
+      <DocListForCurrentSidebarCategory {...props} />
+    );
+  }
+
+  const filteredItems = filterDocCardListItems(items);
+
+  return (
+    <ul className={clsx(className)}>
+    {
+      filteredItems.map((item, index) => {
+        const href = findFirstSidebarItemLink(item);
+
+        return (
+          <li key={index}>
+            <a href={href}>
+              {(item as any).label}
+            </a>
+          </li>
+        );
+      })
+    }
+    </ul>
+  );
+}
