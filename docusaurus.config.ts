ca1f472d71314b10a6ab2178307e59c2647508dc
Aleksey Nemiro <aleksey@kbyte.ru>
2025-01-05 15:17:50 +0300

    docusaurus.config.ts/baseUrl - Set "/knowledgebase/"

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   url: 'https://alekseynemiro.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/knowledgebase/',
   organizationName: 'alekseynemiro',
   projectName: 'knowledgebase',
 
