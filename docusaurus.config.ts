f8fb4877113021ba5720780fda0ade6572b78cdb
Aleksey Nemiro <aleksey@kbyte.ru>
2025-01-05 14:15:15 +0300

    docusaurus.config.ts/url - https://alekseynemiro.github.io

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Aleksey Nemiro\'s knowledge base',
   favicon: 'img/favicon.ico',
-  url: 'https://knowledgebase.nemiro.net',
+  url: 'https://alekseynemiro.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
